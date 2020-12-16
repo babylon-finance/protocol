b5bcbed1c3b295a433f79b6430931605454fe78f
Ramon Recuero <rrmoreno1987@gmail.com>
2020-12-16 09:14:53 -0800

    Adds Fund Token

---
+++ packages/hardhat/contracts/FundToken.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.6.0 <0.7.0;
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+
+contract FundERC20 is ERC20PresetMinterPauser {
+
+  constructor(string name, string symbol) ERC20PresetMinterPauser(name, symbol) public {}
+
+  function burn(address account, uint256 amount) public {
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Only admin can burn");
+    _burn(account, amount);
+  }
+
+  function grantAdminAndRevoke(address newadmin) public {
+    grantRole(DEFAULT_ADMIN_ROLE, newadmin);
+    grantRole(MINTER_ROLE, newadmin);
+    revokeRole(MINTER_ROLE, msg.sender);
+    revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+}
