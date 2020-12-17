84b166231299feed61572c41260b7a857359c769
Tyler Murray <tylerm@allenai.org>
2020-12-17 12:16:36 -0800

    Moves HudgeFund to standalone contract

---
+++ packages/hardhat/contracts/HedgeFund.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./FundToken.sol";
+
+contract HedgeFund {
+    address public manager;
+    bool public active;
+    string public name;
+
+    modifier onlyManager {
+        require(
+            msg.sender == manager,
+            "Only the fund manager can modify fund state"
+        );
+    }
+
+    constructor(
+        string memory _name,
+        bool _active,
+        address _manager
+    ) public {
+        manager = _manager;
+        name = _name;
+        active = _active;
+    }
+
+    function setActive(bool _active) public onlyManager {
+        active = _active;
+    }
+
+    function setManager(address _manager) public onlyManager {
+        manager = _manager;
+    }
+}
--- packages/hardhat/contracts/Holder.sol
+++ packages/hardhat/contracts/Holder.sol
@@ -1,73 +1,95 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./FundToken.sol";
+import "./HedgeFund.sol";
 
 contract Holder {
+    struct HedgeFundMapping {
+        HedgeFund hedgeFund;
+        uint256 index;
+    }
 
-  struct HedgeFund {
-    string name;
-    bool active;
-    uint index;
-  }
+    address public protocolManager;
 
-  address public protocolManager;
+    // Hedge Funds List
+    HedgeFundMapping[] public hedgeFunds;
+    uint256 public currentHedgeFundIndex = 1;
+    uint256 public totalHedgeFunds = 0;
+    mapping(string => uint256) public hedgeFundsMapping;
 
-  // Hedge Funds List
-  HedgeFund[] public hedgeFunds;
-  uint public currentHedgeFundIndex = 1;
-  uint public totalHedgeFunds = 0;
-  mapping (string => uint) public hedgeFundsMapping;
+    // Functions
+    constructor() public {
+        protocolManager = msg.sender;
+    }
 
-  // Functions
-  constructor() public {
-    protocolManager = msg.sender;
-  }
+    modifier onlyProtocol {
+        require(
+            msg.sender == protocolManager,
+            "Only protocol can add strategies"
+        );
+        _;
+    }
 
-  modifier onlyProtocol {
-    require(msg.sender == protocolManager, "Only protocol can add strategies");
-    _;
-  }
+    function addHedgeFund(string memory _name) public onlyProtocol {
+        require(
+            hedgeFundsMapping[_name] == 0,
+            "The hedge fund already exists."
+        );
+        HedgeFund newHedgeFund = new HedgeFund(_name, true, msg.sender);
+        hedgeFunds.push(
+            HedgeFundMapping(newHedgeFund, currentHedgeFundIndex + 1)
+        );
+        hedgeFundsMapping[_name] = currentHedgeFundIndex;
+        currentHedgeFundIndex++;
+        totalHedgeFunds++;
+    }
 
-  function addHedgeFund(string memory name) onlyProtocol public {
-    require(hedgeFundsMapping[name] == 0, "The hedge fund already exists.");
-    hedgeFunds.push(HedgeFund(name, true, currentHedgeFundIndex));
-    hedgeFundsMapping[name] = currentHedgeFundIndex;
-    currentHedgeFundIndex ++;
-    totalHedgeFunds ++;
-  }
+    function disableHedgeFund(string memory _name) public onlyProtocol {
+        uint256 atIndex = hedgeFundsMapping[_name];
+        HedgeFundMapping storage _hedgeFundMapping = hedgeFunds[atIndex - 1];
+        require(
+            _hedgeFundMapping.hedgeFund.active(),
+            "The hedge fund needs to be active."
+        );
+        _hedgeFundMapping.hedgeFund.setActive(false);
+        totalHedgeFunds--;
+    }
 
-  function disableHedgeFund(string memory name) onlyProtocol public {
-    uint atIndex = hedgeFundsMapping[name];
-    HedgeFund storage hedgeFund = hedgeFunds[atIndex - 1];
-    require(hedgeFund.active, "The hedge fund needs to be active.");
-    hedgeFund.active = false;
-    hedgeFund.index = 0;
-    totalHedgeFunds --;
-  }
+    function reenableHedgeFund(string memory _name) public onlyProtocol {
+        uint256 atIndex = hedgeFundsMapping[_name];
+        HedgeFundMapping storage _hedgeFundMapping = hedgeFunds[atIndex - 1];
+        require(
+            !_hedgeFundMapping.hedgeFund.active(),
+            "The hedge fund needs to be disabled."
+        );
+        _hedgeFundMapping.hedgeFund.setActive(true);
+        totalHedgeFunds++;
+    }
 
-  function reenableHedgeFund(string memory name) onlyProtocol public {
-    uint atIndex = hedgeFundsMapping[name];
-    HedgeFund storage hedgeFund = hedgeFunds[atIndex - 1];
-    require(!hedgeFund.active, "The hedge fund needs to be disabled.");
-    hedgeFund.active = true;
-    hedgeFund.index = atIndex;
-    totalHedgeFunds ++;
-  }
+    function getHedgeFund(string memory _name)
+        public
+        view
+        returns (
+            string memory name,
+            bool active,
+            uint256 index
+        )
+    {
+        uint256 atIndex = hedgeFundsMapping[_name];
+        HedgeFundMapping storage _hedgeFundMapping = hedgeFunds[atIndex - 1];
+        return (
+            _hedgeFundMapping.hedgeFund.name(),
+            _hedgeFundMapping.hedgeFund.active(),
+            _hedgeFundMapping.index
+        );
+    }
 
-  function getHedgeFund(string memory _name) public view returns (string memory name, bool active, uint index) {
-    uint atIndex = hedgeFundsMapping[_name];
-    HedgeFund storage hedgeFund = hedgeFunds[atIndex - 1];
-    return (hedgeFund.name, hedgeFund.active, hedgeFund.index);
-  }
-
-  function transferEth(address payable _to, uint amount) private {
-    // Call returns a boolean value indicating success or failure.
-    // This is the current recommended method to use.
-    (bool sent, bytes memory data) = _to.call{value: amount}("");
-    require(sent, "Failed to send Ether");
-  }
-
-
+    function transferEth(address payable _to, uint256 amount) private {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent, ) = _to.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
 }
