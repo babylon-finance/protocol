5a48996f53b2d34c99d61f50b194abaac62d7f88
rriescog <raul@babylon.finance>
2021-03-01 00:47:31 +0100

    WIP BABL Token

---
+++ packages/hardhat/contracts/interfaces/ITimeLock.sol
@@ -0,0 +1,93 @@
+/*
+    Copyright 2020 Babylon Finance
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    SPDX-License-Identifier: Apache License, Version 2.0
+*/
+pragma solidity 0.7.4;
+
+
+interface ITimelock {
+  event NewAdmin(address indexed newAdmin);
+  event NewPendingAdmin(address indexed newPendingAdmin);
+  event NewDelay(uint256 indexed newDelay);
+  event CancelTransaction(
+    bytes32 indexed txHash,
+    address indexed target,
+    uint256 value,
+    string signature,
+    bytes data,
+    uint256 eta
+  );
+  event ExecuteTransaction(
+    bytes32 indexed txHash,
+    address indexed target,
+    uint256 value,
+    string signature,
+    bytes data,
+    uint256 eta
+  );
+  event QueueTransaction(
+    bytes32 indexed txHash,
+    address indexed target,
+    uint256 value,
+    string signature,
+    bytes data,
+    uint256 eta
+  );
+
+  function GRACE_PERIOD() external pure returns (uint256);
+  
+  function MINIMUM_DELAY() external pure returns (uint256);
+  
+  function MAXIMUM_DELAY() external pure returns (uint256);
+
+  function admin() external view returns (address);
+
+  function pendingAdmin() external view returns (address);
+
+  function delay() external view returns (uint256);
+
+  function queuedTransactions(bytes32) external view returns (bool);
+
+  function setDelay(uint256 delay_) external;
+
+  function acceptAdmin() external;
+
+  function setPendingAdmin(address pendingAdmin_) external;
+
+  function queueTransaction(
+    address target,
+    uint256 value,
+    string calldata signature,
+    bytes calldata data,
+    uint256 eta
+  ) external returns (bytes32);
+
+  function cancelTransaction(
+    address target,
+    uint256 value,
+    string calldata signature,
+    bytes calldata data,
+    uint256 eta
+  ) external;
+
+  function executeTransaction(
+    address target,
+    uint256 value,
+    string calldata signature,
+    bytes calldata data,
+    uint256 eta
+  ) external payable returns (bytes memory);
+}
