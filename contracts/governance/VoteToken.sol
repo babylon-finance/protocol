787cae25bdb3c8c608ee90b0fe22fd1f0c8d91fa
Raul Riesco <rriescog@gmail.com>
2021-03-14 17:27:04 +0100

    minor wrong comment erased

--- contracts/governance/VoteToken.sol
+++ contracts/governance/VoteToken.sol
@@ -116,7 +116,7 @@ abstract contract VoteToken is Context, ERC20, Ownable, IVoteToken, ReentrancyGu
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) public override { // TODO - CHECK IF SIGNATURE CAN BE != MSG_SENDER TO AVOID IT
+    ) public override {
         bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name())), getChainId(), address(this)));
         bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
