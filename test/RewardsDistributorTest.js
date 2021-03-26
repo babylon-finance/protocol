0c17ae14716e6f9842d2e5408bd4c53e3502bf1f
Raul Riesco <rriescog@gmail.com>
2021-03-26 14:34:16 +0100

    improving tests

--- test/RewardsDistributorTest.js
+++ test/RewardsDistributorTest.js
@@ -17,64 +17,29 @@ const { BigNumber } = require('@ethersproject/bignumber');
 // `describe` receives the name of a section of your test suite, and a callback.
 // The callback must define the tests of that section. This callback can't be
 // an async function.
+
 describe('BABL Rewards Distributor', function () {
-  // Mocha has four functions that let you hook into the the test runner's
-  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
-
-  // They're very useful to setup the environment for tests, and to clean it
-  // up after they run.
-
-  // A common pattern is to declare some variables, and assign them in the
-  // `before` and `beforeEach` callbacks.
-
-  let controller;
-  let token;
-  let supply;
-  let distributor;
-  let ownerSigner;
-  let userSigner1;
-  let userSigner2;
-  let userSigner3;
+  let babController;
+  let bablToken;
+  let rewardsDistributor;
+  let rewardsSupplySchedule;
+  let strategyDataset;
+  let strategyCandidate;
+  let signer1;
+  let signer2;
+  let signer3;
   let garden1;
   let garden2;
   let garden3;
-  let strategyDataset;
-  let strategyCandidate;
+  let weth;
   let strategy11;
   let strategy21;
-  let weth;
   let balancerIntegration;
 
-  // `beforeEach` will run before each test, re-deploying the contract every
-  // time. It receives a callback, which can be async.
 
   beforeEach(async () => {
-    const {
-      babController,
-      bablToken,
-      rewardsSupplySchedule,
-      rewardsDistributor,
-      owner,
-      signer1,
-      signer2,
-      signer3,
-      garden1,
-      garden2,
-      garden3,
-      strategy11,
-      strategy21,
-      integrationList,
-    } = await loadFixture(deployFolioFixture);
+    ({ babController, bablToken, signer1, garden1, garden2, garden3, strategy11, strategy21, signer2, signer3, balancerIntegration, rewardsDistributor, rewardsSupplySchedule } = await loadFixture(deployFolioFixture));
 
-    balancerIntegration = integrationList;
-    controller = babController;
-    token = bablToken;
-    supply = rewardsSupplySchedule;
-    distributor = rewardsDistributor;
-    ownerSigner = owner;
-    userSigner1 = signer1;
-    userSigner2 = signer2;
-    userSigner3 = signer3;
     weth = await ethers.getContractAt('IERC20', addresses.tokens.WETH);
     strategyDataset = await ethers.getContractAt('Strategy', strategy11);
     strategyCandidate = await ethers.getContractAt('Strategy', strategy21);
@@ -85,14 +50,29 @@ describe('BABL Rewards Distributor', function () {
     // `it` is another Mocha function. This is the one you use to define your
     // tests. It receives the test name, and a callback function.
 
-    it('should successfully deploy BABL Rewards Supply Schedule contract', async function () {
-      const deployedc2 = await supply.deployed();
+    it('should successfully deploy BABL Mining Rewards Supply Schedule contract', async function () {
+      const deployedc2 = await rewardsSupplySchedule.deployed();
       expect(!!deployedc2).to.equal(true);
     });
 
-    it('should successfully deploy BABL Rewards Distributor contract', async function () {
-      const deployedc = await distributor.deployed(supply.address, '50');
+    it('should successfully deploy BABL Mining Rewards Distributor contract', async function () {
+      const deployedc = await rewardsDistributor.deployed(rewardsSupplySchedule.address, '50');
       expect(!!deployedc).to.equal(true);
+    });
+  });
+
+  describe('Garden construction', async function () {
+    it('should have expected properties upon deployment', async function () {
+      expect(await garden1.totalContributors()).to.equal(1);
+      expect(await garden1.creator()).to.equal(await signer1.getAddress());
+      expect(await garden1.controller()).to.equal(babController.address);
+      expect(await garden1.strategyCooldownPeriod()).to.equal(ONE_DAY_IN_SECONDS);
+      expect(await garden1.strategyCreatorProfitPercentage()).to.equal(ethers.utils.parseEther('0.13'));
+      expect(await garden1.strategyVotersProfitPercentage()).to.equal(ethers.utils.parseEther('0.05'));
+      expect(await garden1.gardenCreatorProfitPercentage()).to.equal(ethers.utils.parseEther('0.02'));
+      expect(await garden1.minVotersQuorum()).to.equal(ethers.utils.parseEther('0.10'));
+      expect(await garden1.minIdeaDuration()).to.equal(ONE_DAY_IN_SECONDS * 3);
+      expect(await garden1.maxIdeaDuration()).to.equal(ONE_DAY_IN_SECONDS * 365);
     });
   });
   
