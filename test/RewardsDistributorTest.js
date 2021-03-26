06737d2346f7a4a736d243c0eb34dc0caf09ee55
Raul Riesco <rriescog@gmail.com>
2021-03-26 15:41:02 +0100

    test context

--- test/RewardsDistributorTest.js
+++ test/RewardsDistributorTest.js
@@ -36,9 +36,22 @@ describe('BABL Rewards Distributor', function () {
   let strategy21;
   let balancerIntegration;
 
-
   beforeEach(async () => {
-    ({ babController, bablToken, signer1, garden1, garden2, garden3, strategy11, strategy21, signer2, signer3, balancerIntegration, rewardsDistributor, rewardsSupplySchedule } = await loadFixture(deployFolioFixture));
+    ({
+      babController,
+      bablToken,
+      signer1,
+      garden1,
+      garden2,
+      garden3,
+      strategy11,
+      strategy21,
+      signer2,
+      signer3,
+      balancerIntegration,
+      rewardsDistributor,
+      rewardsSupplySchedule,
+    } = await loadFixture(deployFolioFixture));
 
     weth = await ethers.getContractAt('IERC20', addresses.tokens.WETH);
     strategyDataset = await ethers.getContractAt('Strategy', strategy11);
@@ -75,5 +88,70 @@ describe('BABL Rewards Distributor', function () {
       expect(await garden1.maxIdeaDuration()).to.equal(ONE_DAY_IN_SECONDS * 365);
     });
   });
-  
+
+  describe('Strategies', async function () {
+    it('should return the expected strategy properties by getStrategyDetails()', async function () {
+      const [
+        address,
+        strategist,
+        integration,
+        stake,
+        absoluteTotalVotes,
+        totalVotes,
+        capitalAllocated,
+        duration,
+        expectedReturn,
+        maxCapitalRequested,
+        minRebalanceCapital,
+        enteredAt,
+      ] = await strategyDataset.getStrategyDetails();
+
+      expect(address).to.equal(strategyDataset.address);
+      expect(strategist).to.equal(signer1.address);
+      expect(integration).to.not.equal(addresses.zero);
+      expect(stake).to.equal(ethers.utils.parseEther('5'));
+      expect(absoluteTotalVotes).to.equal(ethers.utils.parseEther('5'));
+      expect(totalVotes).to.equal(ethers.utils.parseEther('5'));
+      expect(capitalAllocated).to.equal(ethers.BigNumber.from(0));
+      expect(duration).to.equal(ethers.BigNumber.from(ONE_DAY_IN_SECONDS * 30));
+      expect(expectedReturn).to.equal(ethers.utils.parseEther('0.05'));
+      expect(maxCapitalRequested).to.equal(ethers.utils.parseEther('10'));
+      expect(minRebalanceCapital).to.equal(ethers.utils.parseEther('1'));
+      expect(enteredAt.isZero()).to.equal(false);
+    });
+  });
+
+  it('should return the expected strategy state by getStrategyState()', async function () {
+    const [address, active, dataSet, finalized, executedAt, exitedAt] = await strategyDataset.getStrategyState();
+
+    expect(address).to.equal(strategyDataset.address);
+    expect(active).to.equal(false);
+    expect(dataSet).to.equal(true);
+    expect(finalized).to.equal(false);
+    expect(executedAt).to.equal(ethers.BigNumber.from(0));
+    expect(exitedAt).to.equal(ethers.BigNumber.from(0));
+  });
+
+  it('should execute investment idea', async function () {
+    ethers.provider.send('evm_increaseTime', [ONE_DAY_IN_SECONDS * 2]);
+
+    const signer1Balance = await garden2.balanceOf(signer1.getAddress());
+    const signer2Balance = await garden2.balanceOf(signer2.getAddress());
+
+    await strategyCandidate.executeInvestment(
+      ethers.utils.parseEther('1'),
+      [signer2.getAddress(), signer3.getAddress()],
+      [signer1Balance, signer2Balance],
+      signer1Balance.add(signer2Balance).toString(),
+      signer1Balance.add(signer2Balance).toString(),
+      {
+        gasPrice: 0,
+      },
+    );
+
+    const [, , , , absoluteTotalVotes, totalVotes] = await strategyCandidate.getStrategyDetails();
+
+    expect(absoluteTotalVotes).to.equal(ethers.utils.parseEther('9.1'));
+    expect(totalVotes).to.equal(ethers.utils.parseEther('9.1'));
+  });
 });
