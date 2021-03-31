2edbb538dd1537064c0ddb25308ab77db9965c16
Ramon Recuero <rrmoreno1987@gmail.com>
2021-03-31 08:39:23 -0700

    Adds Sushiswap integration

---
+++ contracts/integrations/pool/SushiswapPoolIntegration.sol
@@ -0,0 +1,48 @@
+/*
+    Copyright 2021 Babylon Finance
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    SPDX-License-Identifier: Apache License, Version 2.0
+*/
+
+pragma solidity 0.7.4;
+
+import 'hardhat/console.sol';
+import {UniswapPoolIntegration} from './UniswapPoolIntegration.sol';
+
+/**
+ * @title SushiswapPoolIntegration
+ * @author Babylon Finance Protocol
+ *
+ * Sushiswap Protocol pool integration
+ */
+contract SushiswapPoolIntegration is UniswapPoolIntegration {
+
+    /* ============ Constructor ============ */
+
+    /**
+     * Creates the integration
+     *
+     * @param _controller                   Address of the controller
+     * @param _weth                         Address of the WETH ERC20
+     * @param _sushiswapRouterAddress         Address of Sushiswap router
+     */
+    constructor(
+        address _controller,
+        address _weth,
+        address _sushiswapRouterAddress
+    ) UniswapPoolIntegration(_controller, _weth, _sushiswapRouterAddress) {
+        name = "sushiswap_pool";
+    }
+}
