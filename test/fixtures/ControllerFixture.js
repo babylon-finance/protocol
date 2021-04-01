646233f7e87b1139f79eec8af49453d2fed21fff
Ramon Recuero <rrmoreno1987@gmail.com>
2021-04-01 08:28:55 -0700

    OneInchPoolIntegration

--- test/fixtures/ControllerFixture.js
+++ test/fixtures/ControllerFixture.js
@@ -104,6 +104,13 @@ async function deployFolioFixture() {
     addresses.sushiswap.router,
   );
 
+  const OneInchPoolIntegration = await ethers.getContractFactory('OneInchPoolIntegration', owner);
+  const oneInchPoolIntegration = await OneInchPoolIntegration.deploy(
+    babController.address,
+    addresses.tokens.WETH,
+    addresses.oneinch.factory,
+  );
+
   const YearnVaultIntegration = await ethers.getContractFactory('YearnVaultIntegration', owner);
   const yearnVaultIntegration = await YearnVaultIntegration.deploy(
     babController.address,
@@ -120,6 +127,7 @@ async function deployFolioFixture() {
     uniswapPoolIntegration,
     yearnVaultIntegration,
     sushiswapPoolIntegration,
+    oneInchPoolIntegration,
   ];
 
   // Adding integrations
@@ -232,6 +240,7 @@ async function deployFolioFixture() {
     uniswapPoolIntegration,
     yearnVaultIntegration,
     sushiswapPoolIntegration,
+    oneInchPoolIntegration,
 
     garden1,
     garden2,
@@ -259,6 +268,7 @@ async function deployFolioFixture() {
       { name: 'YearnVaultIntegration', contract: yearnVaultIntegration },
       { name: 'UniswapPoolIntegration', contract: uniswapPoolIntegration },
       { name: 'SushiswapPoolIntegration', contract: sushiswapPoolIntegration },
+      { name: 'OneInchPoolIntegration', contract: oneInchPoolIntegration },
     ],
   };
 }
---
+++ test/integrations/OneInchPoolIntegration.test.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai');
+const { waffle, ethers } = require('hardhat');
+const { deployFolioFixture } = require('../fixtures/ControllerFixture');
+const {
+  DEFAULT_STRATEGY_PARAMS,
+  createStrategy,
+  executeStrategy,
+  finalizeStrategy,
+} = require('../fixtures/StrategyHelper');
+const addresses = require('../../utils/addresses');
+const { ADDRESS_ZERO } = require('../../utils/constants');
+
+const { loadFixture } = waffle;
+
+describe('OneInchPoolIntegrationTest', function () {
+  let oneInchPoolIntegration;
+  let garden1;
+  let signer1;
+  let signer2;
+  let signer3;
+  let babController;
+
+  beforeEach(async () => {
+    ({ babController, garden1, oneInchPoolIntegration, signer1, signer2, signer3 } = await loadFixture(
+      deployFolioFixture,
+    ));
+  });
+
+  describe('Deployment', function () {
+    it('should successfully deploy the contract', async function () {
+      const deployed = await babController.deployed();
+      const deployedUni = await oneInchPoolIntegration.deployed();
+      expect(!!deployed).to.equal(true);
+      expect(!!deployedUni).to.equal(true);
+    });
+  });
+
+  describe('Liquidity Pools', function () {
+    let daiWethPair;
+
+    beforeEach(async () => {
+      daiWethPair = await ethers.getContractAt('IMooniswap', addresses.oneinch.pools.wethdai);
+    });
+
+    it('check that a valid pool is valid', async function () {
+      expect(await oneInchPoolIntegration.isPool(addresses.uniswap.pairs.wethdai)).to.equal(true);
+    });
+
+    it('check that an invalid pool is not valid', async function () {
+      await expect(oneInchPoolIntegration.isPool(ADDRESS_ZERO)).to.be.reverted;
+    });
+
+    it('can enter and exit the weth dai pool', async function () {
+      const strategyContract = await createStrategy(
+        1,
+        'vote',
+        [signer1, signer2, signer3],
+        oneInchPoolIntegration.address,
+        garden1,
+        DEFAULT_STRATEGY_PARAMS,
+        [daiWethPair.address],
+      );
+      await executeStrategy(garden1, strategyContract, 0);
+      expect(await daiWethPair.balanceOf(strategyContract.address)).to.be.gt(0);
+
+      await finalizeStrategy(garden1, strategyContract, 0);
+      expect(await daiWethPair.balanceOf(strategyContract.address)).to.equal(0);
+    });
+  });
+});
