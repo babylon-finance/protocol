609ae2d85e9e942ee75af545cd18f0a9e15d6e5d
Tyler Murray <tyler@babylon.finance>
2021-04-19 11:32:09 -0700

    Fixes Min Contrib (#271)

--- test/fixtures/ControllerFixture.js
+++ test/fixtures/ControllerFixture.js
@@ -154,15 +154,15 @@ async function deployFolioFixture() {
   });
 
   const gardenParams = [
-    ethers.utils.parseEther('20'),
-    1,
-    ethers.utils.parseEther('1000'),
-    2,
-    1,
-    ONE_DAY_IN_SECONDS,
-    ethers.utils.parseEther('0.10'), // 10% quorum
-    ONE_DAY_IN_SECONDS * 3,
-    ONE_DAY_IN_SECONDS * 365,
+    ethers.utils.parseEther('20'), // Max Deposit Limit
+    1, // Min Garden Token Supply
+    ethers.utils.parseEther('1000'), // Min Liquidity Asset | ie: Uniswap Volume
+    1, // Deposit Hardlock | 1 second
+    ethers.utils.parseEther('0.10'), // Min Contribution
+    ONE_DAY_IN_SECONDS, // Strategy Cooldown Period
+    ethers.utils.parseEther('0.10'), // Min Voter Quorum | 10%
+    ONE_DAY_IN_SECONDS * 3, // Min Strategy Duration
+    ONE_DAY_IN_SECONDS * 365, // Max Strategy Duration
   ];
 
   // Gives signer1 creator permissions
