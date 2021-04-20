977878f362893caf37efb12e87889e535966ae1c
Igor Yalovoy <to@ylv.io>
2021-04-20 21:46:08 +0300

    Add more test-helpers

--- test/utils/test-helpers.js
+++ test/utils/test-helpers.js
@@ -16,6 +16,7 @@ async function increaseTime(value) {
 /**
  * Can't await on transactionSend because it will never resolve due to
  * automine disabled. Save a promise instead and resolve it later.
+ * Should set gasLimit on txs because default gasLimit is blockGasLimit.
  *   promise = signer.sendTransaction({
  *      to: signer.address,
  *      value: 1,
@@ -25,10 +26,8 @@ async function increaseTime(value) {
  */
 async function mineInBlock(lambda) {
   await ethers.provider.send('evm_setAutomine', [false]);
-  await ethers.provider.send('evm_setIntervalMining', [1000]);
   await lambda();
-  await sleep(1000);
-  await ethers.provider.send('evm_setIntervalMining', [0]);
+  await ethers.provider.send('evm_mine');
   await ethers.provider.send('evm_setAutomine', [true]);
 }
 
