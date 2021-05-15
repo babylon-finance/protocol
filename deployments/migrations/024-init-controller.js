57705bbb487a2b45c4e89c676d74c5b04d492bdd
Ramon Recuero <rrmoreno1987@gmail.com>
2021-05-15 16:04:30 -0700

    Uniswap tweak (#325)
    * Adds threshold to tight twap deviation
    
    * Adds WBTC as the reserve asset

--- deployments/migrations/024-init-controller.js
+++ deployments/migrations/024-init-controller.js
@@ -34,6 +34,8 @@ module.exports = async ({ getNamedAccounts, deployments, ethers, getSigner, getC
   await controllerContract.connect(owner).addReserveAsset('0x6B175474E89094C44Da98b954EedeAC495271d0F', { gasPrice });
   console.log('Adding USDC as reserve asset');
   await controllerContract.connect(owner).addReserveAsset('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', { gasPrice });
+  console.log('Adding WBTC as reserve asset');
+  await controllerContract.connect(owner).addReserveAsset('0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', { gasPrice });
   // Use OpenZeppelin Defender Keeper
   console.log(`Adding KEEPER ${KEEPER}`);
   await controllerContract.connect(owner).addKeepers([KEEPER], { gasPrice });
