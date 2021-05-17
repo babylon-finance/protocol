364e41b9106c248cc9565a72d5f6bd4423fdc102
Igor Yalovoy <to@ylv.io>
2021-05-17 13:55:23 +0300

    Add mainnet artifacts

--- deployments/migrations/028-ownership.js
+++ deployments/migrations/028-ownership.js
@@ -22,6 +22,10 @@ module.exports = async ({ getNamedAccounts, deployments, ethers, getSigner, getC
     '0x71763709Da2488F75bc2DB5d194769d801e97Fa8',
     '0x908295e2be3a36021aadaaed0bbb124fd602cbf2',
     '0xFBbA8ceA4e9835B9f304d6E69905cD9403F2b606',
+    '0x1C4aD6087B14e69a4f8ae378ccEF1DF2A46c671f',
+    '0x1e78164019779F11030e602c02714945a33bA3D5',
+    '0x766e4D47A35d7Ffcc7F4E12ac338697f3e94392B',
+    '0x48d21Dc6BBF18288520E9384aA505015c26ea43C',
   ]) {
     console.log(`Setting creator permission for ${address}`);
     await ishtarGate.connect(deployerSigner).setCreatorPermissions(address, true, { gasPrice });
