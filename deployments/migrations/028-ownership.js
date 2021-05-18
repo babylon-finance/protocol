e01fcd70dbdca062eeaf09105d2545cb1b5e5ee2
rriescog <rriescog@gmail.com>
2021-05-18 16:57:45 +0200

    Changing Raul wallet address for vesting (#332)
    * Changing Raul wallet address for vesting
    
    * Fix test failing in github not local

--- deployments/migrations/028-ownership.js
+++ deployments/migrations/028-ownership.js
@@ -19,7 +19,7 @@ module.exports = async ({ getNamedAccounts, deployments, ethers, getSigner, getC
   for (const address of [
     '0x83f4622A18e38bE297e089fB055Dd5123bb0b279',
     '0x21584Cc5a52102AbB381286a5119E3be08431CfD',
-    '0x71763709Da2488F75bc2DB5d194769d801e97Fa8',
+    '0x232775eAD28F0C0c750A097bA77302E7d84efd3B',
     '0x908295e2be3a36021aadaaed0bbb124fd602cbf2',
     '0xFBbA8ceA4e9835B9f304d6E69905cD9403F2b606',
     '0x1C4aD6087B14e69a4f8ae378ccEF1DF2A46c671f',
