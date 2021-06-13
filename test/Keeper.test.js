796e0e8f08f057f446da6bb337c744ae83cfe23e
Igor Yalovoy <to@ylv.io>
2021-06-14 01:29:39 +0300

    Fix tests

--- test/Keeper.test.js
+++ test/Keeper.test.js
@@ -76,8 +76,23 @@ describe.only('Keeper', function () {
             gasPrice: 0,
           });
 
-        // Keeper gets paid
         expect(await tokenContract.balanceOf(await keeper.getAddress())).to.equal(fee);
+      });
+
+      it(`refuse to pay more than max fee at ${name} garden`, async function () {
+        const garden = await createGarden({ reserveAsset: token });
+        const tokenContract = await ethers.getContractAt('IERC20', token);
+
+        const strategy = await getStrategy({ state: 'deposit', specificParams: addresses.tokens.COMP });
+
+        const signer1Balance = await garden.balanceOf(signer1.getAddress());
+        const signer2Balance = await garden.balanceOf(signer2.getAddress());
+
+        await expect(strategy
+          .connect(keeper)
+          .resolveVoting([signer1.getAddress(), signer2.getAddress()], [signer1Balance, signer2Balance], fee.add(1), {
+            gasPrice: 0,
+          })).to.be.revertedWith(/BAB#019/);
       });
     });
   });
