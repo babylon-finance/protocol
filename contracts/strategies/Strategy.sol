1f2dd41bec6b715996c544586b69f24af45ff068
Ramon Recuero <rrmoreno1987@gmail.com>
2021-06-15 07:57:04 -0700

    Code fix

--- contracts/strategies/Strategy.sol
+++ contracts/strategies/Strategy.sol
@@ -180,7 +180,7 @@ contract Strategy is ReentrancyGuard, IStrategy, Initializable {
     function _onlyUnpaused() private view {
         // Do not execute if Globally or individually paused
         _require(
-            !IBabController(controller).isPaused(address(this)) || msg.sender == msg.sender == controller.owner(),
+            !IBabController(controller).isPaused(address(this)) || msg.sender == controller.owner(),
             Errors.ONLY_UNPAUSED
         );
     }
