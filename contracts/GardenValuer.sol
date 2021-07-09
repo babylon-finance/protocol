0fe8135c598d6666634cc21184e24353e388b2ca
Ramon Recuero <rrmoreno1987@gmail.com>
2021-07-09 06:35:40 -0700

    Substracts rewards in garden valuer (#402)
    * Substracts rewards in garden valuer
    
    * removes adding ETH

--- contracts/GardenValuer.sol
+++ contracts/GardenValuer.sol
@@ -120,11 +120,11 @@ contract GardenValuer {
         // Add garden reserve assets and garden's reserve asset
         valuation = valuation.add(ERC20(reserveAsset).balanceOf(address(_garden)));
 
+        // Subtract the reserves set aside for rewards
+        valuation = valuation.sub(IGarden(_garden).reserveAssetRewardsSetAside());
+
         // Get the valuation in terms of the quote asset
         valuation = valuation.preciseMul(reservePrice);
-
-        // Adds ETH of garden in _quoteAsset prices
-        valuation = valuation.add(address(_garden).balance.preciseMul(wethPrice));
 
         if (quoteAssetDecimals < 18) {
             valuation = valuation.mul(10**(18 - quoteAssetDecimals));
