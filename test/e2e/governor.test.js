a949c06cbf790aaf76c75678519df065ca3865de
Igor Yalovoy <to@ylv.io>
2021-08-19 19:20:51 +0300

    Integration tests

--- test/e2e/governor.test.js
+++ test/e2e/governor.test.js
@@ -3,12 +3,14 @@ const { ethers } = require('hardhat');
 
 const { ADDRESS_ZERO, ONE_DAY_IN_SECONDS } = require('lib/constants');
 const { from, eth, parse } = require('lib/helpers');
-const { increaseTime, voteType, proposalState } = require('utils/test-helpers');
+const { increaseTime, increaseBlock, voteType, proposalState } = require('utils/test-helpers');
+const { getVoters, getGovernorMock, getProposal, castVotes, claimTokens } = require('utils/gov-helpers');
 
 const { setupTests } = require('fixtures/GardenFixture');
 const { impersonateAddress } = require('lib/rpc');
 
-describe('governor', function () {
+describe.only('governor', function () {
+  let deployer;
   let owner;
   let signer1;
   let signer2;
@@ -17,11 +19,13 @@ describe('governor', function () {
   let babGovernor;
   let timelockController;
   let babController;
+  let voters;
 
   const votingPeriod = ONE_DAY_IN_SECONDS * 7;
 
   beforeEach(async () => {
     ({
+      deployer,
       owner,
       signer1,
       signer2,
@@ -31,18 +35,37 @@ describe('governor', function () {
       timelockController,
       babController,
     } = await setupTests()());
+    voters = await getVoters();
   });
+
   describe('upgrades', function () {
-    it('can change governor to a v2', async function () {
+    it('can change governor to a new one', async function () {
+      const mockGovernor = await getGovernorMock(bablToken, deployer, 10);
+      await claimTokens(bablToken, voters);
+      const { id, args } = await getProposal(mockGovernor, bablToken);
+
       // propose
-      await babGovernor
-        .connect(proposer)
-        ['propose(address[],uint256[],bytes[],string)']([target], [value], [encodedData], description);
-      // cast vote
-      await babGovernor.connect(voter).castVote(proposalId, support);
-      // execute
-      await babGovernor.connect(executor).execute(targets, values, calldatas, descriptionHash);
-      // verify
+      await mockGovernor.connect(voters[0])['propose(address[],uint256[],bytes[],string)'](...args);
+
+      // 1 blocks to reach the block where the voting starts
+      await increaseBlock(1);
+
+      const votes = voters.map((vot) => ({ voter: vot, support: voteType.For }));
+      await castVotes(id, votes, mockGovernor);
+
+      // 10 block to skip voting period
+      await increaseBlock(10);
+
+      const [, , eta, , , forVotes, againstVotes, abstainVotes, , ,] = await mockGovernor.proposals(id);
+
+      await mockGovernor.connect(deployer)['queue(uint256)'](id);
+
+      await increaseTime(ONE_DAY_IN_SECONDS);
+
+      await mockGovernor.connect(deployer)['execute(uint256)'](id);
+
+      const state = await mockGovernor.state(id);
+      expect(state).to.eq(proposalState.Executed);
     });
   });
 });
---
+++ test/utils/gov-helpers.js
@@ -0,0 +1,65 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const { ADDRESS_ZERO, ONE_DAY_IN_SECONDS } = require('lib/constants');
+const { from, eth, parse } = require('lib/helpers');
+const { increaseTime, increaseBlock, voteType, proposalState } = require('utils/test-helpers');
+
+const { impersonateAddress } = require('lib/rpc');
+
+export async function getVoters() {
+  return [
+    await impersonateAddress('0x3E7c4E57A1dc4dD4bBE81bEFBe3E437f69619DaB'), // 20K
+    await impersonateAddress('0x06601571AA9D3E8f5f7CDd5b993192618964bAB5'), // 15K
+    await impersonateAddress('0x83f4622A18e38bE297e089fB055Dd5123bb0b279'), // Team 24K
+    await impersonateAddress('0x232775eAD28F0C0c750A097bA77302E7d84efd3B'), // Team 17K
+  ];
+}
+
+// period and delay is in blocks
+export async function getGovernorMock(bablToken, signer, period = 1, delay = 1) {
+  // We deploy a mock contract with a custom period and delay
+  const timelockFactory = await ethers.getContractFactory('TimelockController');
+  const timelock = await timelockFactory.deploy(ONE_DAY_IN_SECONDS, [], []);
+
+  const mockFactory = await ethers.getContractFactory('BabylonGovernorMock');
+  const governor = await mockFactory.deploy(bablToken.address, timelock.address, delay, period);
+
+  await timelock.connect(signer).grantRole(await timelock.PROPOSER_ROLE(), governor.address);
+  await timelock.connect(signer).grantRole(await timelock.EXECUTOR_ROLE(), governor.address);
+
+  return governor;
+}
+
+export async function castVotes(id, votes, governor) {
+  for (const vote of votes) {
+    await governor.connect(vote.voter).castVote(id, from(vote.support));
+  }
+}
+
+export async function claimTokens(bablToken, voters) {
+  for (const voter of voters) {
+    await bablToken.connect(voter).claimMyTokens();
+    await bablToken.connect(voter).delegate(voter.address);
+  }
+}
+
+export async function getProposal(
+  governor,
+  bablToken,
+  { targets, values, calldatas, description } = {
+    targets: [ADDRESS_ZERO],
+    values: [from(0)],
+    calldatas: ['0x'],
+    description: '<proposal description>',
+  },
+) {
+  const descriptionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(description));
+
+  const id = await governor.hashProposal(targets, values, calldatas, descriptionHash);
+
+  return {
+    id,
+    args: [targets, values, calldatas, description],
+  };
+}
