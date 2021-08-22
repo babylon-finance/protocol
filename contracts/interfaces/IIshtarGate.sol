6d908159fa01170e4eaf34f4d5ea99dc75933afb
Ramon Recuero <rrmoreno1987@gmail.com>
2021-08-22 11:57:35 +0200

    MardukGate contract

--- contracts/interfaces/IIshtarGate.sol
+++ contracts/interfaces/IIshtarGate.sol
@@ -8,7 +8,7 @@
     http://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
+    distributedx under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
@@ -17,44 +17,18 @@
 */
 pragma solidity 0.7.6;
 
+import {IBabylonGate} from './IBabylonGate.sol';
+
 /**
  * @title IIshtarGate
  * @author Babylon Finance
  *
  * Interface for interacting with the Gate Guestlist NFT
  */
-interface IIshtarGate {
+interface IIshtarGate is IBabylonGate {
     /* ============ Functions ============ */
-
-    function setGardenAccess(
-        address _user,
-        address _garden,
-        uint8 _permission
-    ) external returns (uint256);
-
-    function setCreatorPermissions(address _user, bool _canCreate) external returns (uint256);
-
-    function grantGardenAccessBatch(
-        address _garden,
-        address[] calldata _users,
-        uint8[] calldata _perms
-    ) external returns (bool);
-
-    function maxNumberOfInvites() external view returns (uint256);
 
     function tokenURI() external view returns (string memory);
 
-    function setMaxNumberOfInvites(uint256 _maxNumberOfInvites) external;
-
     function updateGardenURI(string memory _tokenURI) external;
-
-    function grantCreatorsInBatch(address[] calldata _users, bool[] calldata _perms) external returns (bool);
-
-    function canCreate(address _user) external view returns (bool);
-
-    function canJoinAGarden(address _garden, address _user) external view returns (bool);
-
-    function canVoteInAGarden(address _garden, address _user) external view returns (bool);
-
-    function canAddStrategiesInAGarden(address _garden, address _user) external view returns (bool);
 }
