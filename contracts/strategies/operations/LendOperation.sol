f6cfa1833b77804cfae220a3aeec3bddba467b46
Raul Riesco <rriescog@gmail.com>
2021-08-25 02:58:58 +0200

    Fix math vuln numTokensToRedeem

--- contracts/strategies/operations/LendOperation.sol
+++ contracts/strategies/operations/LendOperation.sol
@@ -197,7 +197,7 @@ contract LendOperation is Operation {
         // Normalize to underlying asset if any (ctokens for compound)
         uint256 numTokensToRedeem = ILendIntegration(_integration).getInvestmentTokenAmount(_sender, _assetToken);
         // Apply percentage
-        numTokensToRedeem = numTokensToRedeem.mul(_percentage.div(10**(18)));
+        numTokensToRedeem = numTokensToRedeem.mul(_percentage).div(10**(18));
         uint256 remainingDebtInCollateralTokens = _getRemainingDebt(_borrowToken, _assetToken, _remaining);
         remainingDebtInCollateralTokens = SafeDecimalMath.normalizeAmountTokens(
             _borrowToken,
