1ac26601b2cf324f041c03399887a977d853e7eb
Ramon Recuero <rrmoreno1987@gmail.com>
2021-08-27 12:59:35 -0400

    Master swapper initial logic

--- contracts/integrations/BaseIntegration.sol
+++ contracts/integrations/BaseIntegration.sol
@@ -52,6 +52,9 @@ abstract contract BaseIntegration {
     IBabController public controller;
     // Wrapped ETH address
     address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     // Name of the integration
     string public name;
 
--- contracts/integrations/trade/MasterSwapper.sol
+++ contracts/integrations/trade/MasterSwapper.sol
@@ -28,6 +28,7 @@ import {ICurveAddressProvider} from '../../interfaces/external/curve/ICurveAddre
 import {ICurveRegistry} from '../../interfaces/external/curve/ICurveRegistry.sol';
 import {ITradeIntegration} from '../../interfaces/ITradeIntegration.sol';
 import {IGarden} from '../../interfaces/IGarden.sol';
+import {IStrategy} from '../../interfaces/IStrategy.sol';
 import {IBabController} from '../../interfaces/IBabController.sol';
 import {BaseIntegration} from '../BaseIntegration.sol';
 
@@ -47,6 +48,19 @@ abstract contract MasterSwapper is BaseIntegration, ReentrancyGuard, ITradeInteg
 
     /* ============ Struct ============ */
 
+    struct TradeInfo {
+        IGarden garden; // Garden
+        address strategy; // Strategy
+        string exchangeName; // Which exchange to use
+        address sendToken; // Address of token being sold
+        address receiveToken; // Address of token being bought
+        uint256 gardenTotalSupply; // Total supply of Garden in Precise Units (10^18)
+        uint256 totalSendQuantity; // Total quantity of sold tokens
+        uint256 totalMinReceiveQuantity; // Total minimum quantity of token to receive back
+        uint256 preTradeSendTokenBalance; // Total initial balance of token being sold
+        uint256 preTradeReceiveTokenBalance; // Total initial balance of token being bought
+    }
+
     /* ============ Events ============ */
 
     /* ============ Constants ============ */
@@ -55,15 +69,26 @@ abstract contract MasterSwapper is BaseIntegration, ReentrancyGuard, ITradeInteg
     ICurveAddressProvider internal constant curveAddressProvider =
         ICurveAddressProvider(0x0000000022D53366457F9d5E68Ec105046FC4383);
 
+    ITradeIntegration internal immutable curve;
+    ITradeIntegration internal immutable univ3;
+    ITradeIntegration internal immutable synthetix;
+
     /* ============ Constructor ============ */
 
     /**
-     * Creates the integration
+     * Creates the master swapper
      *
      * @param _name                   Name of the integration
      * @param _controller             Address of the controller
+     * @param _curve                  Address of curve trade integration
+     * @param _univ3                  Address of uni trade integration
+     * @param _synthetix              Address of synthetix trade integration
      */
-    constructor(string memory _name, IBabController _controller) BaseIntegration('master swapper', _controller) {}
+    constructor(string memory _name, IBabController _controller, ITradeIntegration _curve, ITradeIntegration _univ3, ITradeIntegration _synthetix) BaseIntegration('master swapper', _controller) {
+      curve = _curve;
+      univ3 = _univ3;
+      synthetix = _synthetix;
+    }
 
     /* ============ External Functions ============ */
 
@@ -84,16 +109,99 @@ abstract contract MasterSwapper is BaseIntegration, ReentrancyGuard, ITradeInteg
         address _receiveToken,
         uint256 _minReceiveQuantity
     ) external override nonReentrant {
-        ICurveRegistry curveRegistry = ICurveRegistry(curveAddressProvider.get_registry());
-        address curvePool = curveRegistry.find_pool_for_coins(_sendToken, _receiveToken, 0);
-        if (curvePool != address(0)) {
-            // Swap on curve
+        TradeInfo memory tradeInfo =
+            _createTradeInfo(_strategy, name, _sendToken, _receiveToken, _sendQuantity, _minReceiveQuantity);
+        // Curve
+        bool found = _checkAllCurvePaths(tradeInfo);
+        if (found) {
+          return;
         }
-        // Check Synthetix
-        // Swap on Univ3
+        // Synthetix
+
         // Swap on synthetix
+        // Swap on Univ3
         // Swap on univ2 (only direct trade)
     }
 
     /* ============ Internal Functions ============ */
+
+    /**
+     * Create and return TradeInfo struct
+     *
+     * @param _strategy             Address of the strategy
+     * @param _exchangeName         Human readable name of the exchange in the integrations registry
+     * @param _sendToken            Address of the token to be sent to the exchange
+     * @param _receiveToken         Address of the token that will be received from the exchange
+     * @param _sendQuantity         Units of token in SetToken sent to the exchange
+     * @param _minReceiveQuantity   Min units of token in SetToken to be received from the exchange
+     *
+     * return TradeInfo             Struct containing data for trade
+     */
+    function _createTradeInfo(
+        address _strategy,
+        string memory _exchangeName,
+        address _sendToken,
+        address _receiveToken,
+        uint256 _sendQuantity,
+        uint256 _minReceiveQuantity
+    ) internal view returns (TradeInfo memory) {
+        TradeInfo memory tradeInfo;
+
+        tradeInfo.strategy = _strategy;
+        tradeInfo.garden = IStrategy(tradeInfo.strategy).garden();
+
+        tradeInfo.exchangeName = _exchangeName;
+
+        tradeInfo.sendToken = _sendToken;
+        tradeInfo.receiveToken = _receiveToken;
+
+        tradeInfo.gardenTotalSupply = ERC20(address(tradeInfo.garden)).totalSupply();
+
+        tradeInfo.totalSendQuantity = _sendQuantity;
+
+        tradeInfo.totalMinReceiveQuantity = _minReceiveQuantity;
+
+        tradeInfo.preTradeSendTokenBalance = ERC20(_sendToken).balanceOf(_strategy);
+        tradeInfo.preTradeReceiveTokenBalance = ERC20(_receiveToken).balanceOf(_strategy);
+
+        return tradeInfo;
+    }
+
+    function _checkAllCurvePaths(TradeInfo memory _tradeInfo) private returns (bool) {
+      ICurveRegistry curveRegistry = ICurveRegistry(curveAddressProvider.get_registry());
+      bool found = false;
+      address curvePool = curveRegistry.find_pool_for_coins(_tradeInfo.sendToken, _tradeInfo.receiveToken);
+      // Direct pairs via curve
+      if (curvePool != address(0)) {
+         ITradeIntegration(curve).trade(_tradeInfo.strategy, _tradeInfo.sendToken, _tradeInfo.totalSendQuantity, _tradeInfo.receiveToken, _tradeInfo.totalMinReceiveQuantity);
+         return true;
+      }
+      found = _checkCurveRoutesThroughReserve(WETH, _tradeInfo, curveRegistry);
+      if (!found) {
+        found = _checkCurveRoutesThroughReserve(DAI, _tradeInfo, curveRegistry);
+        if (!found) {
+          found = _checkCurveRoutesThroughReserve(WBTC, _tradeInfo, curveRegistry);
+        }
+      }
+      return found;
+    }
+
+    function _checkCurveRoutesThroughReserve(address _reserve, TradeInfo memory _tradeInfo, ICurveRegistry curveRegistry) private returns (bool) {
+      uint256 reserveBalance = ERC20(_reserve).balanceOf(_tradeInfo.strategy);
+      // Going through curve but switching first to reserve
+      address curvePool = curveRegistry.find_pool_for_coins(_reserve, _tradeInfo.receiveToken);
+      if (curvePool != address(0)) {
+          ITradeIntegration(univ3).trade(_tradeInfo.strategy, _tradeInfo.sendToken,  _tradeInfo.totalSendQuantity, _reserve, 0);
+          ITradeIntegration(curve).trade(_tradeInfo.strategy, _reserve, ERC20(_reserve).balanceOf(_tradeInfo.strategy).sub(reserveBalance), _tradeInfo.receiveToken, _tradeInfo.totalMinReceiveQuantity);
+          return true;
+      }
+      // Going through curve to reserve and then receive Token
+      curvePool = curveRegistry.find_pool_for_coins(_tradeInfo.sendToken, _reserve);
+      if (curvePool != address(0)) {
+          ITradeIntegration(curve).trade(_tradeInfo.strategy, _tradeInfo.sendToken,  _tradeInfo.totalSendQuantity, _reserve, 0);
+          ITradeIntegration(univ3).trade(_tradeInfo.strategy, _reserve, ERC20(_reserve).balanceOf(_tradeInfo.strategy).sub(reserveBalance), _tradeInfo.receiveToken, _tradeInfo.totalMinReceiveQuantity);
+          return true;
+      }
+      return false;
+    }
 }
