872d8f77c14654b502583b4155a8170899fd3092
Igor Yalovoy <to@ylv.io>
2021-09-03 16:51:42 -0400

    Test setut.js

---
+++ test/utils/setup.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+
+const { increaseTime, increaseBlock, getTimestamp, mineInBlock } = require('utils/test-helpers');
+const { setup } = require('../../lib/scripts/setup');
+const { setupTests } = require('fixtures/GardenFixture');
+
+describe.only('setup', function () {
+  beforeEach(async () => {
+    await setupTests()();
+  });
+
+  it('runs', async function () {
+    const hre = require('hardhat');
+    await setup(hre);
+  });
+});
