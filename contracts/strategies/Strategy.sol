0a34bd9128bdbe68cdd34ebd46d388f898a7bf4e
Ramon Recuero <rrmoreno1987@gmail.com>
2021-09-08 16:37:17 -0400

    Removes

--- contracts/strategies/Strategy.sol
+++ contracts/strategies/Strategy.sol
@@ -643,12 +643,6 @@ contract Strategy is ReentrancyGuard, IStrategy, Initializable {
         address reserveAsset = garden.reserveAsset();
         for (uint256 i = 0; i < opTypes.length; i++) {
             IOperation operation = IOperation(IBabController(controller).enabledOperations(uint256(opTypes[i])));
-            // If next operation is a stake, only count the value of the last one
-            if (opTypes.length > i + 1) {
-                if (opTypes[i] < 3 && opTypes[i + 1] == 2) {
-                    continue;
-                }
-            }
             // _getOpDecodedData guarantee backward compatibility with OpData
             try operation.getNAV(_getOpDecodedData(i), garden, opIntegrations[i]) returns (
                 uint256 opNAV,
