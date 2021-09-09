c554d6b9127cdce12bcd25bdd06d26b0592e8fa3
Ramon Recuero <rrmoreno1987@gmail.com>
2021-09-09 16:09:12 -0400

    lend fix

--- contracts/strategies/operations/LendOperation.sol
+++ contracts/strategies/operations/LendOperation.sol
@@ -164,22 +164,24 @@ contract LendOperation is Operation {
             SafeDecimalMath.normalizeAmountTokens(lendToken, _garden.reserveAsset(), assetTokenAmount).preciseDiv(
                 price
             );
-        address rewardsToken = ILendIntegration(_integration).getRewardToken();
-        uint256 rewardsAmount = ILendIntegration(_integration).getRewardsAccrued(msg.sender);
-        if (rewardsAmount > 0) {
-            uint256 priceRewards = _getPrice(_garden.reserveAsset(), rewardsToken);
-            // We add rewards
-            if (priceRewards != 0) {
-                NAV = NAV.add(
-                    SafeDecimalMath
-                        .normalizeAmountTokens(
-                        ILendIntegration(_integration).getRewardToken(),
-                        _garden.reserveAsset(),
-                        rewardsAmount
-                    )
-                        .preciseDiv(priceRewards)
-                );
-            }
+        address rewardsToken = _getRewardToken(_integration);
+        if (rewardsToken != address(0)) {
+          uint256 rewardsAmount = ILendIntegration(_integration).getRewardsAccrued(msg.sender);
+          if (rewardsAmount > 0) {
+              uint256 priceRewards = _getPrice(_garden.reserveAsset(), rewardsToken);
+              // We add rewards
+              if (priceRewards != 0) {
+                  NAV = NAV.add(
+                      SafeDecimalMath
+                          .normalizeAmountTokens(
+                          rewardsToken,
+                          _garden.reserveAsset(),
+                          rewardsAmount
+                      )
+                          .preciseDiv(priceRewards)
+                  );
+              }
+          }
         }
         require(NAV != 0, 'NAV has to be bigger 0');
         return (NAV, true);
@@ -237,13 +239,12 @@ contract LendOperation is Operation {
                 _garden.reserveAsset()
             );
         }
-        try ILendIntegration(_integration).getRewardToken() returns (address rewardsToken) {
-            uint256 rewardsBalance = IERC20(rewardsToken).balanceOf(_sender);
-            // Add rewards
-            if (rewardsBalance > 1e16) {
-                IStrategy(_sender).trade(rewardsToken, rewardsBalance, _garden.reserveAsset());
-            }
-        } catch {}
+        address rewardsToken = _getRewardToken(_integration);
+        uint256 rewardsBalance = IERC20(rewardsToken).balanceOf(_sender);
+        // Add rewards
+        if (rewardsBalance > 1e16) {
+            IStrategy(_sender).trade(rewardsToken, rewardsBalance, _garden.reserveAsset());
+        }
     }
 
     function _getRemainingDebt(
@@ -256,5 +257,13 @@ contract LendOperation is Operation {
         }
         uint256 price = _getPrice(_borrowToken, _assetToken);
         return _remaining.preciseMul(price);
+    }
+
+    function _getRewardToken(address _integration) private view returns (address) {
+      try ILendIntegration(_integration).getRewardToken() returns (address rewardsToken) {
+          return rewardsToken;
+      } catch {
+        return address(0);
+      }
     }
 }
