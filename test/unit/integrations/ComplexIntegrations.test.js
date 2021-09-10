30c6a8a41c0e5edabec52aab1c699f727db68f24
Ramon Recuero <rrmoreno1987@gmail.com>
2021-09-10 09:09:28 -0400

    Adds complex tests

--- test/unit/integrations/ComplexIntegrations.test.js
+++ test/unit/integrations/ComplexIntegrations.test.js
@@ -4,12 +4,19 @@ const { createStrategy, executeStrategy, finalizeStrategy, DAI_STRATEGY_PARAMS }
 const { setupTests } = require('fixtures/GardenFixture');
 const { createGarden, depositFunds, transferFunds } = require('fixtures/GardenHelper');
 const addresses = require('lib/addresses');
+const { ADDRESS_ZERO } = require('lib/constants');
 const { from, eth, parse } = require('lib/helpers');
 
 describe('ComplexIntegrationsTest', function () {
   let aaveBorrowIntegration;
   let aaveLendIntegration;
   let uniswapV3TradeIntegration;
+<<<<<<< Updated upstream
+=======
+  let compoundBorrowIntegration;
+  let compoundLendIntegration;
+  let masterSwapper;
+>>>>>>> Stashed changes
   let signer1;
   let signer2;
   let signer3;
@@ -22,6 +29,9 @@ describe('ComplexIntegrationsTest', function () {
       aaveBorrowIntegration,
       uniswapV3TradeIntegration,
       aaveLendIntegration,
+      compoundBorrowIntegration,
+      compoundLendIntegration,
+      masterSwapper,
       signer1,
       signer2,
       signer3,
@@ -33,7 +43,7 @@ describe('ComplexIntegrationsTest', function () {
   });
 
   describe('getNAV', function () {
-    it(`of a leveraged ETH (AaveLend->AaveBorrow->BuyOp)`, async function () {
+    it(`DAI Garden of a leveraged ETH (AaveLend WETH->AaveBorrow DAI->BuyOp WETH)`, async function () {
       await transferFunds(dai.address);
 
       const garden = await createGarden({ reserveAsset: dai.address });
@@ -44,7 +54,7 @@ describe('ComplexIntegrationsTest', function () {
         'custom',
         'vote',
         [signer1, signer2, signer3],
-        [aaveLendIntegration.address, aaveBorrowIntegration.address, uniswapV3TradeIntegration.address],
+        [aaveLendIntegration.address, aaveBorrowIntegration.address, masterSwapper.address],
         garden,
         DAI_STRATEGY_PARAMS,
         [weth.address, 0, dai.address, 0, weth.address, 0],
@@ -54,6 +64,62 @@ describe('ComplexIntegrationsTest', function () {
 
       const nav = await strategyContract.getNAV();
       expect(nav).to.be.closeTo(eth(1000), eth(20));
+    });
+
+    it(`DAI Garden (CompLend WETH->CompBorrow USDC->BuyOp DPI)`, async function () {
+      await transferFunds(dai.address);
+
+      const garden = await createGarden({ reserveAsset: dai.address });
+
+      await depositFunds(dai.address, garden);
+      const DPI = '0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b';
+      console.log('Lend weth', weth.address.toString());
+      console.log('Borrow usdc', usdc.address.toString());
+      console.log('Buy DPI', DPI.toString());
+      const strategyContract = await createStrategy(
+        'custom',
+        'vote',
+        [signer1, signer2, signer3],
+        [compoundLendIntegration.address, compoundBorrowIntegration.address, masterSwapper.address],
+        garden,
+        DAI_STRATEGY_PARAMS,
+        // [weth.address, 0, usdc.address, 0, DPI, 0],
+        [ADDRESS_ZERO, 0, dai.address, 0, DPI, 0],
+        [3, 4, 0],
+      );
+      console.log('Strategy created');
+      await executeStrategy(strategyContract);
+      console.log('EXECUTED');
+      // const nav = await strategyContract.getNAV();
+      // expect(nav).to.be.closeTo(eth(1000), eth(20));
+    });
+
+    it(`DAI Garden (AaveLend WETH->AaveBorrow USDC->BuyOp DPI)`, async function () {
+      await transferFunds(dai.address);
+
+      const garden = await createGarden({ reserveAsset: dai.address });
+
+      await depositFunds(dai.address, garden);
+      const DPI = '0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b';
+      console.log('Lend weth', weth.address.toString());
+      console.log('Borrow usdc', usdc.address.toString());
+      console.log('Buy DPI', DPI.toString());
+      const strategyContract = await createStrategy(
+        'custom',
+        'vote',
+        [signer1, signer2, signer3],
+        [aaveLendIntegration.address, aaveBorrowIntegration.address, masterSwapper.address],
+        garden,
+        DAI_STRATEGY_PARAMS,
+        // [weth.address, 0, usdc.address, 0, DPI, 0],
+        [weth.address, 0, dai.address, 0, DPI, 0],
+        [3, 4, 0],
+      );
+      console.log('Strategy created');
+      await executeStrategy(strategyContract);
+      console.log('EXECUTED');
+      // const nav = await strategyContract.getNAV();
+      // expect(nav).to.be.closeTo(eth(1000), eth(20));
     });
   });
 });
