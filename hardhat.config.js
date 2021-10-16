ea9da839441031def2cffd2ca9398d3fea684724
Igor Yalovoy <to@ylv.io>
2021-10-16 18:04:09 +0300

    Add live strategies script

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ require('./lib/tasks/gate');
 require('./lib/tasks/increase-time');
 require('./lib/tasks/upgrade-admin');
 require('./lib/tasks/upgrade-beacon');
-require('./lib/tasks/migrate-users');
+require('./lib/tasks/add-live-strategies');
 require('./lib/tasks/tvl');
 require('./lib/tasks/users');
 require('./lib/tasks/gardens');
