dd634427825b349ec5ba72ea4bd23df1f74f835c
Ramon Recuero <rrmoreno1987@gmail.com>
2021-10-21 13:48:47 -0700

    viewer updates

--- contracts/BabylonViewer.sol
+++ contracts/BabylonViewer.sol
@@ -195,7 +195,7 @@ contract BabylonViewer {
         returns (
             address,
             string memory,
-            uint256[14] memory,
+            uint256[15] memory,
             bool[] memory,
             uint256[] memory
         )
