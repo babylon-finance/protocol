bb25406834c9437d68dad20cd6133533df88bd00
Ramon Recuero <rrmoreno1987@gmail.com>
2021-10-23 14:53:45 -0700

    Test fixes

--- test/deploy/deploy.test.js
+++ test/deploy/deploy.test.js
@@ -1,132 +1,145 @@
 const { expect } = require('chai');
-const { deployments } = require('hardhat');
-
-const { impersonateAddress } = require('lib/rpc');
-const { ONE_DAY_IN_SECONDS } = require('lib/constants.js');
+// const { deployments } = require('hardhat');
+// const { increaseTime } = require('utils/test-helpers');
+// const { deploy } = deployments;
 const addresses = require('lib/addresses');
-const { fund } = require('lib/whale');
 const { takeSnapshot, restoreSnapshot } = require('lib/rpc');
-const { from, parse, eth } = require('lib/helpers');
+const { eth } = require('lib/helpers');
 const { getContracts, deployFixture } = require('lib/deploy');
-const { increaseTime } = require('utils/test-helpers');
 
-const { deploy } = deployments;
+const STUCK_EXECUTE = [
+  // '0x69ef15D3a4910EDc47145f6A88Ae60548F5AbC2C',
+  // '0xcd9498b4160568DeEAb0fE3A0De739EbF152CB48',
+  // '0xE064ad71dc506130A4C1C85Fb137606BaaCDe9c0', // Long BED
+  // '0x702c284Cd32F842bE450f5e5C9DE48f14303F1C8', // Long TOKE. Reason: Error: execution reverted: BAB#098
+  // '0x5fF64AB324806aBDb8902Ff690B90a078D36CCe1', // Long wbtc, borrow DAI, long CDT. Reason: Error: execution reverted: Master swapper could not swap
+
+  // '0x6F854a988577Ce994926a8979881E6a18E6a70dF', // lend wbtc, borrow dai, long LDO. Reason: Error: execution reverted: Curve Swap failed midway
+  // '0x81b1C6A04599b910e33b1AB549DE4a19E5701838', // Lend wbtc, borrow dai, yield yearn dai. Reason: Error: execution reverted: Curve Swap failed midway
+  '0xc38E5828c1c84F4687f2080c0C8d2e4a89695A11', // Long eth, borrow dai, steth crv convex. Reason: Error: execution reverted: The garden did not receive the investment tokens
+  // '0x19C54aDcfAB5a3608540130418580176d325c1F9', // Eth 3x. Reason: Error: execution reverted: Address: low-level call with value failed -> No liquidity
+];
 
 describe('deploy', function () {
   let owner;
   let gov;
   let keeper;
-  let priceOracle;
   let gardens;
   let strategyNft;
   let valuer;
   let gardensNAV;
   let snapshotId;
 
-  async function canUnwindAllActiveStrategies() {
+  async function iterateStrategiesFromGardens(cb) {
     for (const garden of gardens) {
       const gardenContract = await ethers.getContractAt('Garden', garden);
       console.log(`${await gardenContract.name()}`);
 
       const reserveAsset = await gardenContract.reserveAsset();
-
       const strategies = await gardenContract.getStrategies();
       for (const strategy of strategies) {
         const strategyContract = await ethers.getContractAt('IStrategy', strategy, owner);
-        const isExecuting = await strategyContract.isStrategyActive();
         const name = await strategyNft.getStrategyName(strategy);
 
-        if (!isExecuting) {
-          console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
-          continue;
-        }
-
-        console.log(`  Unwinding capital ${name} ${strategyContract.address}`);
-
-        try {
-          const capital = reserveAsset === addresses.tokens.DAI ? eth(3000) : eth(1);
-          await strategyContract.connect(owner).unwindStrategy(capital);
-
-          const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
-
-          expect(active).eq(true);
-          expect(finalized).eq(false);
-          expect(exitedAt).eq(0);
-        } catch (e) {
-          console.log(`  failed to unwind capital ${e}`);
-        }
+        cb(strategyContract, name, reserveAsset);
       }
     }
+  }
+
+  async function unwindStrategy(strategyContract, name, reserveAsset) {
+    const isExecuting = await strategyContract.isStrategyActive();
+    if (!isExecuting) {
+      console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
+      return;
+    }
+    console.log(`  Unwinding capital ${name} ${strategyContract.address}`);
+    try {
+      const capital = reserveAsset === addresses.tokens.DAI ? eth(3000) : eth(1);
+      await strategyContract.connect(owner).unwindStrategy(capital);
+
+      const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
+
+      expect(active).eq(true);
+      expect(finalized).eq(false);
+      expect(exitedAt).eq(0);
+    } catch (e) {
+      console.log(`  failed to unwind capital ${e}`);
+    }
+  }
+
+  async function addCapitalToStrategy(strategyContract, name, reserveAsset) {
+    const isExecuting = await strategyContract.isStrategyActive();
+    if (isExecuting) {
+      console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
+    }
+
+    console.log(`  Adding capital to the strategy ${name} ${strategyContract.address}`);
+
+    try {
+      const capital = reserveAsset === addresses.tokens.DAI ? eth(3000) : eth(1);
+      await strategyContract.connect(keeper).executeStrategy(capital, 1);
+
+      const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
+
+      expect(active).eq(true);
+      expect(finalized).eq(false);
+      expect(exitedAt).eq(0);
+    } catch (e) {
+      console.log(`  failed to allocate capital to the strategy ${e}`);
+    }
+  }
+
+  async function finalizeStrategy(strategyContract, name, reserveAsset) {
+    const isExecuting = await strategyContract.isStrategyActive();
+    if (!isExecuting) {
+      console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
+      return;
+    }
+
+    console.log(`  Finalizing strategy ${name} ${strategyContract.address}`);
+    await strategyContract
+      .connect(gov)
+      .updateParams([
+        1,
+        strategyContract.maxGasFeePercentage(),
+        strategyContract.maxTradeSlippagePercentage(),
+        strategyContract.maxAllocationPercentage(),
+      ]);
+    console.log('updated params');
+    try {
+      await strategyContract.connect(keeper).finalizeStrategy(1, '');
+
+      const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
+
+      expect(active).eq(false);
+      expect(finalized).eq(true);
+      expect(exitedAt).gt(0);
+    } catch (e) {
+      console.log(`failed to finalize strategy ${e}`);
+    }
+  }
+
+  async function executeStuckStrategies() {
+    const strategies = STUCK_EXECUTE;
+    for (const strategy of strategies) {
+      const strategyContract = await ethers.getContractAt('IStrategy', strategy, owner);
+      const gardenContract = await ethers.getContractAt('Garden', strategyContract.garden());
+      const reserveAsset = await gardenContract.reserveAsset();
+      const name = await strategyNft.getStrategyName(strategy);
+      addCapitalToStrategy(strategyContract, name, reserveAsset);
+    }
+  }
+
+  async function canUnwindAllActiveStrategies() {
+    iterateStrategiesFromGardens(unwindStrategy);
   }
 
   async function canAllocateCapitalToAllActiveStrategies() {
-    for (const garden of gardens) {
-      const gardenContract = await ethers.getContractAt('Garden', garden);
-      console.log(`${await gardenContract.name()}`);
-
-      const reserveAsset = await gardenContract.reserveAsset();
-
-      const strategies = await gardenContract.getStrategies();
-      for (const strategy of strategies) {
-        const strategyContract = await ethers.getContractAt('IStrategy', strategy, owner);
-        const isExecuting = await strategyContract.isStrategyActive();
-        const name = await strategyNft.getStrategyName(strategy);
-
-        if (!isExecuting) {
-          console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
-          continue;
-        }
-
-        console.log(`  Adding capital to the strategy ${name} ${strategyContract.address}`);
-
-        try {
-          const capital = reserveAsset === addresses.tokens.DAI ? eth(3000) : eth(1);
-          await strategyContract.connect(keeper).executeStrategy(capital, 1);
-
-          const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
-
-          expect(active).eq(true);
-          expect(finalized).eq(false);
-          expect(exitedAt).eq(0);
-        } catch (e) {
-          console.log(`  failed to allocate capital to the strategy ${e}`);
-        }
-      }
-    }
+    iterateStrategiesFromGardens(addCapitalToStrategy);
   }
 
   async function canFinalizeAllActiveStrategies() {
-    for (const garden of gardens) {
-      const gardenContract = await ethers.getContractAt('Garden', garden);
-      console.log(`${await gardenContract.name()}`);
-
-      const strategies = await gardenContract.getStrategies();
-      for (const strategy of strategies) {
-        const strategyContract = await ethers.getContractAt('IStrategy', strategy, owner);
-        const isExecuting = await strategyContract.isStrategyActive();
-        const name = await strategyNft.getStrategyName(strategy);
-
-        if (!isExecuting) {
-          console.log(`  Strategy ${name} ${strategyContract.address} is not active.`);
-          continue;
-        }
-
-        console.log(`  Finalizing strategy ${name} ${strategyContract.address}`);
-        await strategyContract.connect(gov).changeStrategyDuration(1);
-
-        try {
-          await strategyContract.connect(keeper).finalizeStrategy(1, '');
-
-          const [, active, , finalized, , exitedAt] = await strategyContract.getStrategyState();
-
-          expect(active).eq(false);
-          expect(finalized).eq(true);
-          expect(exitedAt).gt(0);
-        } catch (e) {
-          console.log(`failed to finalize strategy ${e}`);
-        }
-      }
-    }
+    iterateStrategiesFromGardens(finalizeStrategy);
   }
 
   describe('before deployment', function () {
@@ -146,11 +159,15 @@ describe('deploy', function () {
       await canUnwindAllActiveStrategies();
     });
 
+    it.skip('can execute stuck proposals', async () => {
+      await executeStuckStrategies();
+    });
+
     it.skip('can allocate all active strategies', async () => {
       await canAllocateCapitalToAllActiveStrategies();
     });
 
-    it('can finalize all active strategies', async () => {
+    it.only('can finalize all active strategies', async () => {
       await canFinalizeAllActiveStrategies();
     });
   });
@@ -159,10 +176,10 @@ describe('deploy', function () {
   // TODO: Check that gardens can start new strategies with all integrations
   describe('after deployment', function () {
     beforeEach(async () => {
-      ({ owner, gov, keeper, priceOracle, gardens, gardensNAV, strategyNft, valuer } = await deployFixture());
+      ({ owner, gov, keeper, gardens, gardensNAV, strategyNft, valuer } = await deployFixture());
     });
 
-    it.only('NAV has NOT changed for gardens after deploy', async () => {
+    it('NAV has NOT changed for gardens after deploy', async () => {
       for (const garden of gardens) {
         const gardenContract = await ethers.getContractAt('Garden', garden);
         const gardenNAV = (await valuer.calculateGardenValuation(garden, addresses.tokens.DAI))
@@ -173,6 +190,14 @@ describe('deploy', function () {
         );
         expect(gardenNAV).to.closeTo(gardensNAV[garden], eth());
       }
+    });
+
+    it.skip('can execute stuck proposals', async () => {
+      await executeStuckStrategies();
+    });
+
+    it('can allocate all active strategies', async () => {
+      await canAllocateCapitalToAllActiveStrategies();
     });
 
     it('can finalize all active strategies', async () => {
