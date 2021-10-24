0af48a69915b269852d7d7b1909c9618f5e67612
rriescog <rriescog@gmail.com>
2021-10-25 01:27:25 +0200

    fix

--- contracts/gardens/Garden.sol
+++ contracts/gardens/Garden.sol
@@ -500,6 +500,11 @@ contract Garden is ERC20Upgradeable, ReentrancyGuard, IGarden {
         _onlyContributor();
         Contributor storage contributor = contributors[msg.sender];
         _require(block.timestamp > contributor.claimedAt, Errors.ALREADY_CLAIMED); // race condition check
+        // Flashloan protection
+        _require(
+            block.timestamp.sub(contributor.lastDepositAt) >= depositHardlock,
+            Errors.DEPOSIT_HARDLOCK
+        );
         uint256[] memory rewards = new uint256[](7);
 
         rewards = rewardsDistributor.getRewards(address(this), msg.sender, _finalizedStrategies);
