94e612f0e8d45db633ee44c21cab43a3cef355d7
rriescog <rriescog@gmail.com>
2021-10-25 15:41:11 +0200

    fix (#559)

--- contracts/gardens/Garden.sol
+++ contracts/gardens/Garden.sol
@@ -901,7 +901,7 @@ contract Garden is ERC20Upgradeable, ReentrancyGuard, IGarden {
         uint256 amountOut = _sharesToReserve(_amountIn, _pricePerShare);
 
         // if withPenaltiy then unwind strategy
-        if (_withPenalty) {
+        if (_withPenalty && !(liquidReserve() >= amountOut)) {
             amountOut = amountOut.sub(amountOut.preciseMul(EARLY_WITHDRAWAL_PENALTY));
             // When unwinding a strategy, a slippage on integrations will result in receiving less tokens
             // than desired so we have have to account for this with a 5% slippage.
