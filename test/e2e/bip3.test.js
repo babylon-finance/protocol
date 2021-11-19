4cba4591ab57b0bec0b824db248894b8c189472c
Igor Yalovoy <to@ylv.io>
2021-11-19 17:33:56 +0300

    Fix tests

--- test/e2e/bip3.test.js
+++ test/e2e/bip3.test.js
@@ -34,11 +34,8 @@ describe('BIP3', function () {
 
     it.only('can execute bip3', async () => {
       const governor = await ethers.getContractAt('BabylonGovernor', '0xBEC3de5b14902C660Bd2C7EfD2F259998424cc24');
-      const bablToken = await ethers.getContractAt('BABLToken', '0xF4Dc48D260C93ad6a96c5Ce563E70CA578987c74');
-
       const timelock = '0xe6Ed0eAcB79a6e457416E4df38ed778fd6C6D193';
       const ownerV2 = await impersonateAddress('0x0B892EbC6a4bF484CDDb7253c6BD5261490163b9');
-      await bablToken.connect(ownerV2).transferOwnership(timelock);
 
       await increaseBlock(await governor.votingPeriod());
 
