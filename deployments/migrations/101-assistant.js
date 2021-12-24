f3dcdd87e3919911777dc6b177ff4864f3cd64ae
Igor Yalovoy <to@ylv.io>
2021-12-24 19:18:24 +0300

    Add Assistant SC

---
+++ deployments/migrations/101-assistant.js
@@ -0,0 +1,33 @@
+module.exports = async ({
+  getNamedAccounts,
+  deployments,
+  upgradesDeployer,
+  getGasPrice,
+  network,
+  tenderly,
+  getTenderlyContracts,
+}) => {
+  const { deployer } = await getNamedAccounts();
+  const signers = await ethers.getSigners();
+  const chainId = await getChainId();
+  const signer = await getSigner(deployer);
+  const gasPrice = await getGasPrice();
+
+  const deployment = await upgradesDeployer.deployAdminProxy(
+    'Assistant',
+    'AssistantProxy',
+    { from: deployer, log: true, gasPrice },
+    {
+      initializer: { method: 'initialize', args: [] },
+    },
+  );
+
+  console.log('Verify on Etherscan');
+  await hre.run('verify:verify', {
+    contract: 'contracts/governance/Assistant.sol:Assistant',
+    address: (await deployments.get('Assistant')).address,
+    constructorArguments: [],
+  });
+};
+
+module.exports.tags = ['Assistant'];
