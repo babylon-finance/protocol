e0598a77cb576ba05ea80cd4190ebc0e5367d840
Ramon Recuero <rrmoreno1987@gmail.com>
2022-01-04 10:29:18 +0100

    Blacklists BABL in the constructor

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -75,6 +75,7 @@ contract PriceOracle is Ownable, IPriceOracle {
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address internal constant BABL = 0xF4Dc48D260C93ad6a96c5Ce563E70CA578987c74;
     address private constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     IStETH private constant stETH = IStETH(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
     IWstETH private constant wstETH = IWstETH(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
@@ -118,6 +119,8 @@ contract PriceOracle is Ownable, IPriceOracle {
         tokenIdentifier = _tokenIdentifier;
         controller = _controller;
         maxTwapDeviation = INITIAL_TWAP_DEVIATION;
+        // Blacklist babl as an oracle reserve
+        blackListReserveForOracle[BABL] = true;
         _updateReserves();
     }
 
