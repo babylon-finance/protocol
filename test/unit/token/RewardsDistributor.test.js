148355f4443969256aee7c5bb151509449b6213f
rriescog <29550529+rriescog@users.noreply.github.com>
2022-01-07 12:43:37 +0100

    remove only

--- test/unit/token/RewardsDistributor.test.js
+++ test/unit/token/RewardsDistributor.test.js
@@ -1306,7 +1306,7 @@ async function getStrategyState(strategy) {
     });
   });
 
-  describe.only('Capital reallocation and unwinding per garden-reserveAsset', function () {
+  describe('Capital reallocation and unwinding per garden-reserveAsset', function () {
     [
       { token: addresses.tokens.WETH, name: 'WETH' },
       //  { token: addresses.tokens.DAI, name: 'DAI' }, cannot trade the same asset DAI for DAI
