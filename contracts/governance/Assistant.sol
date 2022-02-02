448f99e04ae931e628ccb743c466d2458679ab58
Ramon Recuero <rrmoreno1987@gmail.com>
2022-02-02 15:26:51 -0800

    BIP-10 (#627)

--- contracts/governance/Assistant.sol
+++ contracts/governance/Assistant.sol
@@ -55,44 +55,10 @@ contract Assistant is OwnableUpgradeable {
     }
 
     /* ============ External Functions ============ */
-    function setupFusePool() external onlyGovernanceOrEmergency {
+    function startHeart() external onlyGovernanceOrEmergency {
         IERC20 BABL = IERC20(0xF4Dc48D260C93ad6a96c5Ce563E70CA578987c74);
-        address DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        address WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        address treasury = 0xD7AAf4676F0F52993cb33aD36784BF970f0E1259;
-        ISwapRouter swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-        uint256 daiAmount = 90000e18; // 90k
-        uint256 bablAmount = 2000e18; // 2k
-        // 1. Change all ETH to WETH
-        IWETH(WETH9).deposit{value: address(this).balance}();
 
-        // Approve the router to spend DAI.
-        TransferHelper.safeApprove(DAI, address(swapRouter), daiAmount);
-        // Swap all DAI for WETH in univ3
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH9,
-                fee: 500, // 0.05%
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: daiAmount, //90k DAI
-                amountOutMinimum: 22e18, //24 ETH out min
-                sqrtPriceLimitX96: 0
-            });
-        // 2. The call to `exactInputSingle` executes the swap.
-        uint256 amountOut = swapRouter.exactInputSingle(params);
-        require(amountOut >= 22e18);
-        uint256 ethAmount = 655e17; // 65.5 ETH
-        //3. Fund new hypervisor on visor
-        IHypervisor visor = IHypervisor(0x5e6c481dE496554b66657Dd1CA1F70C61cf11660);
-        BABL.safeApprove(address(visor), bablAmount);
-        IERC20(WETH9).safeApprove(address(visor), ethAmount);
-        uint256 shares = visor.deposit(ethAmount, bablAmount, treasury);
-        require(shares == visor.balanceOf(treasury) && visor.balanceOf(treasury) > 0, 'Not enough lp tokens');
-
-        // 4. Send remaining WETH back to the treasury
-        IERC20(WETH9).safeTransferFrom(address(this), treasury, IERC20(WETH9).balanceOf(address(this)));
+        BABL.safeTransferFrom(address(this), 0x97FcC2Ae862D03143b393e9fA73A32b563d57A6e, 10000e18);
     }
 
     /* ============ External Getter Functions ============ */
