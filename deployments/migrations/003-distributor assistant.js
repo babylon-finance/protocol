4e4cad8922b28ce05baef4383a3542b5c022a7a9
rriescog <29550529+rriescog@users.noreply.github.com>
2022-02-03 02:30:57 +0100

    fix deployment script broke after merge

--- deployments/migrations/003-distributor assistant.js
+++ deployments/migrations/003-distributor assistant.js
@@ -12,22 +12,26 @@ module.exports = async ({
   const { deploy } = deployments;
   const { deployer, owner } = await getNamedAccounts();
   const signer = await getSigner(deployer);
-  const gasPrice = await getGasPrice();
+  const { maxPriorityFeePerGas } = await getGasPrice();
+  console.log('check 0');
+  const contract = 'RewardsAssistant';
 
   const rewardsDistributor = await getContract('RewardsDistributor', 'RewardsDistributorProxy');
   const controller = await getController();
-
-  const rewardsAssistant = await deploy('RewardsAssistant', {
+  console.log('check 1');
+  const rewardsAssistant = await deploy(contract, {
     from: deployer,
     args: [controller.address],
     log: true,
-    gasPrice,
+    maxPriorityFeePerGas,
   });
+  console.log('check 2');
 
   if (rewardsAssistant.newlyDeployed) {
     console.log(`Setting rewards assistant on rewards distributor ${rewardsAssistant.address}`);
-    await (await rewardsDistributor.setRewardsAssistant(rewardsAssistant.address, { gasPrice })).wait();
+    await (await rewardsDistributor.setRewardsAssistant(rewardsAssistant.address, { maxPriorityFeePerGas })).wait();
   }
+  console.log('check 3');
 
   if (network.live && rewardsDistributor.newlyDeployed) {
     // fails, mostly likely because of the usage of libs
