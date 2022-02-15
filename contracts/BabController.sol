d7e9329e85c9ba46dd0d4a8c495ce9f190bf9a2a
Igor Yalovoy <to@ylv.io>
2022-02-15 18:50:49 +0300

    Fix Controller

--- contracts/BabController.sol
+++ contracts/BabController.sol
@@ -347,9 +347,6 @@ contract BabController is OwnableUpgradeable, IBabController {
         require(!validReserveAsset[_reserveAsset], 'Reserve asset already added');
         validReserveAsset[_reserveAsset] = true;
         reserveAssets.push(_reserveAsset);
-        if (priceOracle != address(0)) {
-            IPriceOracle(priceOracle).updateReserves();
-        }
         emit ReserveAssetAdded(_reserveAsset);
     }
 
