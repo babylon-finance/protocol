98d0aed0b0080875033665fdcc665d098db7fbe3
Igor Yalovoy <to@ylv.io>
2022-02-18 17:37:25 -0800

    Fix tests

--- test/unit/gardens/Garden.test.js
+++ test/unit/gardens/Garden.test.js
@@ -1630,30 +1630,30 @@ describe('Garden', function () {
       await garden
         .connect(signer1)
         .updateGardenParams([
-          from(1),
-          from(2),
+          eth(1),
+          eth(2),
           from(3),
-          from(4),
-          from(5),
-          from(6),
-          from(7),
-          from(8),
+          eth(4),
+          ONE_DAY_IN_SECONDS * 5,
+          eth(0.06),
+          ONE_DAY_IN_SECONDS * 7,
+          ONE_DAY_IN_SECONDS * 8,
           from(9),
           from(10),
           from(11),
         ]);
 
-      expect(await garden.maxDepositLimit()).to.eq(1);
-      expect(await garden.minLiquidityAsset()).to.eq(2);
+      expect(await garden.maxDepositLimit()).to.eq(eth());
+      expect(await garden.minLiquidityAsset()).to.eq(eth(2));
       expect(await garden.depositHardlock()).to.eq(3);
-      expect(await garden.minContribution()).to.eq(4);
-      expect(await garden.strategyCooldownPeriod()).to.eq(5);
-      expect(await garden.minVotesQuorum()).to.eq(6);
-      expect(await garden.minStrategyDuration()).to.eq(7);
-      expect(await garden.maxStrategyDuration()).to.eq(8);
+      expect(await garden.minContribution()).to.eq(eth(4));
+      expect(await garden.strategyCooldownPeriod()).to.eq(ONE_DAY_IN_SECONDS * 5);
+      expect(await garden.minVotesQuorum()).to.eq(eth(0.06));
+      expect(await garden.minStrategyDuration()).to.eq(ONE_DAY_IN_SECONDS * 7);
+      expect(await garden.maxStrategyDuration()).to.eq(ONE_DAY_IN_SECONDS * 8);
       expect(await garden.minVoters()).to.eq(9);
       expect(await garden.pricePerShareDecayRate()).to.eq(10);
-      expect(await garden.pricePerShareSlippage()).to.eq(11);
+      expect(await garden.pricePerShareDelta()).to.eq(11);
     });
   });
 
