167b69197b2a148e07559933a1d6891b14df9b80
rriescog <29550529+rriescog@users.noreply.github.com>
2022-02-21 09:46:57 -0800

    FIX BIP11 Assistant code (#654)
    * fix bip11
    
    * Prettify code
    
    * Refactor
    
    * Artifacts
    
    Co-authored-by: Igor Yalovoy <to@ylv.io>

--- contracts/governance/Assistant.sol
+++ contracts/governance/Assistant.sol
@@ -25,6 +25,8 @@ import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/Own
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
+import 'hardhat/console.sol';
+
 import {IWETH} from '../interfaces/external/weth/IWETH.sol';
 import {IHypervisor} from '../interfaces/IHypervisor.sol';
 import {IBabController} from '../interfaces/IBabController.sol';
@@ -41,8 +43,6 @@ contract Assistant is OwnableUpgradeable {
     /* ============ State Variables ============ */
     /* ============ Constants ============ */
 
-    IBabController controller = IBabController(0xD4a5b5fcB561dAF3aDF86F8477555B92FBa43b5F);
-
     /* ============ Constructor ============ */
 
     function initialize() public {
@@ -52,6 +52,10 @@ contract Assistant is OwnableUpgradeable {
     /* ============ External Functions ============ */
 
     function startBondingProgram() external {
+        IBabController controller = IBabController(0xD4a5b5fcB561dAF3aDF86F8477555B92FBa43b5F);
+
+        require(msg.sender == controller.owner(), 'not valid sender');
+
         IERC20 BABL = IERC20(0xF4Dc48D260C93ad6a96c5Ce563E70CA578987c74);
         BABL.safeTransfer(controller.heart(), 11000e18);
     }
@@ -64,4 +68,4 @@ contract Assistant is OwnableUpgradeable {
     receive() external payable {}
 }
 
-contract AssistantV6 is Assistant {}
+contract AssistantV7 is Assistant {}
