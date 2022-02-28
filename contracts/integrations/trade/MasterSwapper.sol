25eb9b1660bdf441339f89f28efd20d475fa4ac5
Ramon Recuero <rrmoreno1987@gmail.com>
2022-02-28 10:53:36 -0800

    Paladin Trade integration

--- contracts/integrations/trade/MasterSwapper.sol
+++ contracts/integrations/trade/MasterSwapper.sol
@@ -211,13 +211,18 @@ contract MasterSwapper is BaseIntegration, ReentrancyGuard, ITradeIntegration {
             uint256 aaveBalance = ERC20(AAVE).balanceOf(_strategy);
             ITradeIntegration(univ3).trade(_strategy, _sendToken, _sendQuantity, AAVE, 1);
             aaveBalance = ERC20(AAVE).balanceOf(_strategy).sub(aaveBalance);
-            ITradeIntegration(paladinTradeIntegration).trade(
+            try ITradeIntegration(paladinTradeIntegration).trade(
                 _strategy,
                 AAVE,
                 aaveBalance,
                 palStkAAVE,
                 _minReceiveQuantity
-            );
+            ) {
+              return;
+            } catch Error(string memory _err) {
+                error = _formatError(error, _err, 'Paladin Trade Integration ', _sendToken, WETH);
+            }
+
         }
 
         // Heart Direct
