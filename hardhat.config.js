dc4189b63f6d6cdf7c325fa9d4f4f6e0c1a34c66
Igor Yalovoy <to@ylv.io>
2022-02-28 16:34:36 -0800

    Bump Solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -113,16 +113,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: OPTIMIZER,
-            runs: 999,
-          },
-        },
-      },
-      {
-        version: '0.8.2',
+        version: '0.8.9',
         settings: {
           optimizer: {
             enabled: OPTIMIZER,
