10f510a67a2e8b070b3d869ef1c97bd99216f115
Ramon Recuero <rrmoreno1987@gmail.com>
2022-03-10 20:58:26 -0800

    strategy fix

--- contracts/strategies/Strategy.sol
+++ contracts/strategies/Strategy.sol
@@ -375,7 +375,7 @@ contract Strategy is ReentrancyGuard, IStrategy, Initializable {
         uint256 reserveAssetReturns = IERC20(garden.reserveAsset()).balanceOf(address(this));
         // Execute exit operations
         _exitStrategy(HUNDRED_PERCENT);
-        capitalReturned = IERC20(garden.reserveAsset()).balanceOf(address(this)).sub(reserveAssetReturns);
+        capitalReturned = IERC20(garden.reserveAsset()).balanceOf(address(this));
         // Mark as finalized
         finalized = true;
         active = false;
