d3125669f525e5cbd852995cbdf98bca919ec802
Igor Yalovoy <to@ylv.io>
2022-03-27 17:13:34 +0100

    Revert "fix"
    This reverts commit aceda18bca343f22e95ed3b5b554fb85af401220.

--- contracts/governance/Assistant.sol
+++ contracts/governance/Assistant.sol
@@ -12,14 +12,12 @@ import {IHypervisor} from '../interfaces/IHypervisor.sol';
 import {IBabController} from '../interfaces/IBabController.sol';
 
 import {ControllerLib} from '../lib/ControllerLib.sol';
-import {LowGasSafeMath} from '../lib/LowGasSafeMath.sol';
 
 import 'hardhat/console.sol';
 
 contract Assistant is OwnableUpgradeable {
     using SafeERC20 for IERC20;
     using ControllerLib for IBabController;
-    using LowGasSafeMath for uint256;
 
     /* ============ Events ============ */
     /* ============ Modifiers ============ */
@@ -35,15 +33,17 @@ contract Assistant is OwnableUpgradeable {
 
     /* ============ External Functions ============ */
 
-    // We need to send total BABL to assistant first
+    function setupUIGovernor() external {}
+
+    // We need to send BABL to assistant first
     function rewardProphets(address[] calldata _addresses) external {
-        uint256 bablPerProphet = 2e18;
         require(address(msg.sender) == 0x97FcC2Ae862D03143b393e9fA73A32b563d57A6e, 'Only multisig');
         IERC20 BABL = IERC20(0xF4Dc48D260C93ad6a96c5Ce563E70CA578987c74);
+        // BABL.safeTransferFrom(msg.sender, address(this), 2e18 * _addresses.length);
         uint256 length = _addresses.length;
-        require(BABL.balanceOf(address(this)) >= length.mul(bablPerProphet), 'Not enough BABL');
-        for (uint256 i = 0; i < length; i++) {
-            BABL.safeTransfer(_addresses[i], bablPerProphet);
+        for (uint256 i = 0; i < length;) {
+            BABL.transfer(_addresses[i], 2e18);
+            ++i;
         }
     }
 
