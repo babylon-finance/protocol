e58471aa1e8296bbff4ab75bf7ffde05a7360df0
Igor Yalovoy <to@ylv.io>
2022-04-13 19:25:10 -0400

    Allow to pass rateMode

--- contracts/strategies/Strategy.sol
+++ contracts/strategies/Strategy.sol
@@ -457,11 +457,12 @@ contract Strategy is ReentrancyGuard, IStrategy, Initializable {
     /**
      * Emergency fix to change interest rate
      */
-    function swapInterestRate() external {
+    function swapInterestRate(uint256 rateMode) external {
+        _onlyStrategistOrGovernor();
         if (address(this) == 0x371B23eEdb1a5E3822AaCFf906187111A91fAE88) {
             ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9).swapBorrowRateMode(
                 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
-                1
+                rateMode
             );
         }
     }
