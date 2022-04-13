e31c0db9d179c02217968bde9a56263aa788adce
Igor Yalovoy <to@ylv.io>
2022-04-13 18:52:16 -0400

    Remove modifier at swapInterestRate

--- contracts/strategies/Strategy.sol
+++ contracts/strategies/Strategy.sol
@@ -457,7 +457,6 @@ contract Strategy is ReentrancyGuard, IStrategy, Initializable {
      * Emergency fix to change interest rate
      */
     function swapInterestRate() external {
-        _onlyStrategistOrGovernor();
         if (address(this) == 0x371B23eEdb1a5E3822AaCFf906187111A91fAE88) {
             ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9).swapBorrowRateMode(
                 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
